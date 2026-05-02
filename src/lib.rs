064e83940eb870c8237a97180879fd0071128a9b
karoterra <karoterra2008@gmail.com>
2026-05-02 23:27:03 +0900

    :sparkles: `.typ` ファイルをドラッグアンドドロップできるようにハンドラを追加

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,15 @@ mod typst_world;
 
 use std::path::Path;
 
-use aviutl2::{AnyResult, anyhow::anyhow, tracing};
+use aviutl2::{AnyResult, anyhow, tracing};
 
 use crate::path::DLL_DIR;
 use crate::typst_file_plugin::TypstFilePlugin;
 use crate::typst_text_plugin::TypstTextPlugin;
 use crate::typst_world::TYPST_ENGINE;
+
+static PLUGIN_NAME: &str = "Typst for AviUtl2";
+static EDIT_HANDLE: aviutl2::generic::GlobalEditHandle = aviutl2::generic::GlobalEditHandle::new();
 
 #[aviutl2::plugin(GenericPlugin)]
 struct TypstPlugin {
@@ -25,7 +28,7 @@ impl aviutl2::generic::GenericPlugin for TypstPlugin {
         Self::init_logging();
 
         if DLL_DIR.is_none() {
-            return Err(anyhow!("Failed to determine DLL directory."));
+            return Err(anyhow::anyhow!("Failed to determine DLL directory."));
         }
         tracing::debug!("DLL directory: {:?}", DLL_DIR.as_ref().unwrap());
 
@@ -37,17 +40,68 @@ impl aviutl2::generic::GenericPlugin for TypstPlugin {
 
     fn plugin_info(&self) -> aviutl2::generic::GenericPluginTable {
         aviutl2::generic::GenericPluginTable {
-            name: "Typst for AviUtl2".to_string(),
+            name: PLUGIN_NAME.to_string(),
             information: format!(
-                "Typst for AviUtl2 v{} by karoterra",
+                "{} v{} by karoterra",
+                PLUGIN_NAME,
                 env!("CARGO_PKG_VERSION")
             ),
         }
     }
 
     fn register(&mut self, registry: &mut aviutl2::generic::HostAppHandle) {
+        EDIT_HANDLE.init(registry.create_edit_handle());
+
         registry.register_filter_plugin(&self.text_plugin);
         registry.register_filter_plugin(&self.file_plugin);
+
+        let filters = aviutl2::file_filters! {
+            "Typst file" => ["typ"]
+        };
+        registry.register_file_drop_handler(PLUGIN_NAME, &filters, |path| {
+            let res = EDIT_HANDLE
+                .call_edit_section(|edit_section| {
+                    let position = edit_section.get_mouse_layer_frame()?.unwrap_or(
+                        aviutl2::generic::LayerFrameData {
+                            layer: edit_section.info.layer,
+                            frame: edit_section.info.frame,
+                        },
+                    );
+
+                    let mut object_alias = aviutl2::alias::Table::new();
+                    let mut object = aviutl2::alias::Table::new();
+                    object.insert_value(
+                        "name",
+                        path.file_name()
+                            .and_then(|n| n.to_str())
+                            .unwrap_or("Typstファイル"),
+                    );
+                    let mut object_0 = aviutl2::alias::Table::new();
+                    object_0.insert_value("effect.name", "Typstファイル");
+                    object_0.insert_value(
+                        "ファイル",
+                        path.to_str().ok_or(anyhow::anyhow!(
+                            "Failed to convert path to string: {path:?}"
+                        ))?,
+                    );
+                    object.insert_table("0", object_0);
+                    object_alias.insert_table("Object", object);
+
+                    edit_section.create_object_from_alias(
+                        &object_alias.to_string(),
+                        position.layer,
+                        position.frame,
+                        0,
+                    )?;
+
+                    anyhow::Ok(())
+                })
+                .map_err(anyhow::Error::from)
+                .flatten();
+            if let Err(e) = res {
+                tracing::error!("Failed to handle file drop: {e}");
+            }
+        });
     }
 
     fn on_project_load(&mut self, _project: &mut aviutl2::generic::ProjectFile) {
